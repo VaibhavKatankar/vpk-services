b578168a6d6bd0a247a3503e0405f7d1392bd825
VaibhavKatankar <katankarvaibhav31@gmail.com>
2026-03-08 13:11:50 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mysql = require("mysql");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -7,41 +6,33 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-// Database connection
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "service_db"
-});
+// Database connection - commented out for now
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "service_db"
+// });
 
-db.connect(function(err) {
-    if (err) {
-        console.log("Database error:", err);
-    } else {
-        console.log("Database connected");
-    }
-});
+// db.connect(function(err) {
+//     if (err) {
+//         console.log("Database error:", err);
+//     } else {
+//         console.log("Database connected");
+//     }
+// });
 
-// Save order
+// Save order - mock implementation
 app.post("/order", function(req, res) {
 
     const name = req.body.name;
     const service = req.body.service;
     const address = req.body.address;
 
-    const sql = "INSERT INTO orders (name, service, address) VALUES (?, ?, ?)";
+    console.log("Order received:", { name, service, address });
 
-    db.query(sql, [name, service, address], function(err, result) {
-
-        if (err) {
-            console.log(err);
-            res.send("Error saving order");
-        } else {
-            res.send("Order submitted successfully");
-        }
-
-    });
+    // Mock response
+    res.send("Order submitted successfully");
 
 });
 
